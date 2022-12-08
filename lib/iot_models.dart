1e749d8c5dfd25b99ad5d6cda7774cfbc1e22e31
Nikita Masev <n.masev@bastion-tech.ru>
2022-12-08 21:28:24 +0300

    empty project;

---
+++ lib/iot_models.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library iot_models;
+
+//export 'src/iot_models_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
