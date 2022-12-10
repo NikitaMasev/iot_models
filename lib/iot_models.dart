731dd51ad67acc9abd73b303ff4ceb80edde2458
Nikita Masev <n.masev@bastion-tech.ru>
2022-12-10 13:25:12 +0300

    updated export file;

--- lib/iot_models.dart
+++ lib/iot_models.dart
@@ -1,11 +1,15 @@
 library iot_models;
 
+export 'adapters/communicator_sign_decoder.dart';
+export 'adapters/communicator_sign_decoder_impl.dart';
 export 'converters/iot_device_data_converter.dart';
 export 'models/base/communicator_sign.dart';
 export 'models/base/type_device.dart';
 export 'models/client.dart';
+export 'models/client_event.dart';
 export 'models/control_data.dart';
 export 'models/dynamic_data/lamp_data.dart';
 export 'models/dynamic_data/ups_data.dart';
 export 'models/iot_device.dart';
 export 'models/iot_devices_data_wrapper.dart';
+//export 'models/iot_event.dart';
